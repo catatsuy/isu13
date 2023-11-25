0e541e36c518176415f41b92fcbae9b30d40bd06
catatsuy <catatsuy@catatsuy.org>
2023-11-25 12:13:15 +0900

    Add interpolate params to MySQL connection configuration

---
+++ go/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+echo "start deploy ${USER}"
+GOOS=linux GOARCH=amd64 go build -o isupipe_linux -ldflags "-s -w"
+for server in isu-app; do
+  ssh -t $server "sudo systemctl stop isupipe-go"
+  scp ./isupipe_linux $server:/home/isucon/webapp/go/isupipe
+  ssh -t $server "sudo systemctl start isupipe-go"
+done
+
+echo "finish deploy ${USER}"
