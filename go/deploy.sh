d822859007781de35208a8fe2cc838b466f293b5
catatsuy <catatsuy@catatsuy.org>
2023-12-03 13:32:57 +0900

    mod deploy

--- go/deploy.sh
+++ go/deploy.sh
@@ -4,7 +4,7 @@ set -x
 
 echo "start deploy ${USER}"
 GOOS=linux GOARCH=amd64 go build -o isupipe_linux -ldflags "-s -w"
-for server in isu-app; do
+for server in i1; do
   ssh -t $server "sudo systemctl stop isupipe-go"
   scp ./isupipe_linux $server:/home/isucon/webapp/go/isupipe
   ssh -t $server "sudo systemctl start isupipe-go"
